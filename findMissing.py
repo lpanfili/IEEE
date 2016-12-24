dc0ca6bebbea3e2d16a64a83f7033d7abffbca5e
lpanfili <lpanfili@uw.edu>
2016-12-24 10:21:54 -0800

    Now finds extra files as well as missing files

--- findMissing.py
+++ findMissing.py
@@ -7,12 +7,30 @@ import os
 speakerID = input("Speaker ID: ")
 print("Missing files for", speakerID, ":")
 count = 0
+extracount = 0
+sentIDs = []
+fileIDs = []
 
 for i in range(1, 73):
 	for j in range (1, 11):
 		sentID = '{0}_{1}'.format(i, j)
-		path = '/Users/laurapanfili/Desktop/fall-2016/NIH/recordings-2016/{0}/{0}_seg/{0}_{1}_{2}.wav'.format(speakerID, i, j)
+		sentIDs.append(sentID)	
+		path = '/Users/Laura/Desktop/fall-2016/NIH/recordings-2016/{0}/{0}_seg/{0}_{1}_{2}.wav'.format(speakerID, i, j)
 		if not os.path.exists(path):
 			count += 1
 			print(sentID)
 print(count, "missing sentences")
+
+for sent in sentIDs:
+	fileID = "{0}_{1}.wav".format(speakerID, sent)
+	fileIDs.append(fileID)
+
+print("Extra files for", speakerID, ":")
+for root, dirs, files in os.walk("/Users/Laura/Desktop/fall-2016/NIH/recordings-2016/{0}/{0}_seg".format(speakerID), topdown=False):
+	for filename in files:
+		if filename != ".DS_Store":
+			filename = os.path.basename(filename)
+			if filename not in fileIDs:
+					extracount += 1
+					print(filename)
+print(extracount, "extra sentences")
